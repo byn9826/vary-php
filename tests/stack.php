941d172d1bdf2484b6261d96f285e924ea3c28fa
byn9826 <byn9826@gmail.com>
2019-01-07 15:47:57 -0500

    Data Structure: Stack Class

--- tests/stack.php
+++ tests/stack.php
@@ -1,5 +1,81 @@
 <?php
-$stack = new \Vary\Stack();
-if ($stack->size() !== 0) {
-  echo "Stack size method broken";
+$test_array1 = [
+  0, 1, 2, null, true, false, [], [1], [1, '1', false], 
+  [1, ['1']], new \Vary\Stack(), [1, [new \Vary\Stack()]] 
+];
+$stack1 = new \Vary\Stack();
+if ($stack1->size() !== 0) {
+  throw new Exception('Stack1 initial error');
+}
+foreach($test_array1 as $case) {
+  if ($stack1->push($case) == false) {
+    throw new Exception('Stack 1 push error');
+  }
+}
+if ($stack1->size() !== 12) {
+  throw new Exception('Stack1 size error');
+}
+
+$test_array2 = [
+  0, 1, 2, null, true, false, [], [1], [1, '1', false], 
+  [1, ['1']], new \Vary\Stack(), [1, [new \Vary\Stack()]] 
+];
+$stack2 = new \Vary\Stack();
+if ($stack2->size() !== 0) {
+  throw new Exception('Stack2 initial error');
+}
+foreach($test_array2 as $key => $case) {
+  if ($stack2->push($case) == false) {
+    throw new Exception('Stack2 push error');
+  }
+  if ($stack2->size() - 1 !== $key) {
+    throw new Exception('Stack2 size error');
+  }
+}
+if ($stack2->size() !== 12) {
+  throw new Exception('Stack2 final error');
+}
+
+$stack3 = new \Vary\Stack([
+  0, 1, 2, null, true, false, [], [1], [1, '1', false], 
+  [1, ['1']], new \Vary\Stack(), [1, [new \Vary\Stack()]] 
+]);
+if ($stack3->size() !== count($stack2->_items)) {
+  throw new Exception('Stack3 initial error');
+}
+if ($stack3->size() !== 12) {
+  throw new Exception('Stack3 final error');
+}
+
+$test_array4 = [0, 1, 2, null, true, false];
+$stack4 = new \Vary\Stack([0, 1, 2, null, true, false]);
+if ($stack4->size() !== 6) {
+  throw new Exception('Stack4 initial error');
+}
+foreach($test_array4 as $key => $case) {
+  $return = $stack4->pop();
+  if ($return !== $test_array4[count($test_array4) - 1 - $key]) {
+    throw new Exception('Stack4 pop error');
+  }
+}
+
+$test_array5 = [0, 1, 2, null, true, false];
+$stack5 = new \Vary\Stack();
+foreach($test_array5 as $key => $case) {
+  if ($stack4->push($case) == false) {
+    throw new Exception('Stack5 push error');
+  }
+  $stack5->pop();
+  if ($stack5->size() !== 0) {
+    throw new Exception('Stack5 size error');
+  }
+}
+
+$test_array6 = [0, 1, 2, null, true, false];
+$stack6 = new \Vary\Stack([0, 1, 2, null, true, false]);
+foreach($test_array6 as $case) {
+  $stack6->pop();
+}
+if ($stack6->size() !== 0) {
+  throw new Exception('Stack6 wrong size');
 }
--- vary.c
+++ vary.c
@@ -1,5 +1,4 @@
 /* vary extension for PHP */
-
 #ifdef HAVE_CONFIG_H
 # include "config.h"
 #endif
@@ -10,14 +9,37 @@
 
 zend_class_entry *vary_stack_ce;
 
-ZEND_BEGIN_ARG_INFO_EX(arginfo_void, 0, 0, 0)
+ZEND_BEGIN_ARG_INFO(arginfo_void, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO(arginfo_array_init, 0)
+  ZEND_ARG_INFO(0, array)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO(arginfo_any, 0)
+  ZEND_ARG_INFO(0, value)
 ZEND_END_ARG_INFO()
 
 PHP_METHOD(Stack, __construct)
 {
-  if (zend_parse_parameters_none() == FAILURE) { return; }
-  zval _items;
-  array_init(&_items);
+  zval _items, *param;
+  ZEND_PARSE_PARAMETERS_START(0, 1)
+    Z_PARAM_OPTIONAL
+    Z_PARAM_ARRAY(param)
+  ZEND_PARSE_PARAMETERS_END();
+  int num_args = ZEND_CALL_NUM_ARGS(execute_data);
+  if (num_args > 1) {
+    php_error_docref(
+      NULL,
+      E_WARNING,
+      "\\Vary\\Stack only use the first parameter"
+    );
+    ZVAL_COPY(&_items, param);
+  } else if (num_args == 1) {
+    ZVAL_COPY(&_items, param);
+  } else if (num_args == 0) {
+    array_init(&_items);
+  }
   zend_update_property(
     vary_stack_ce,
     getThis(),
@@ -26,11 +48,12 @@ PHP_METHOD(Stack, __construct)
     &_items TSRMLS_CC
   );
   zval_ptr_dtor(&_items);
+  RETURN_TRUE;
 }
 
 PHP_METHOD(Stack, size)
 {
-  if (zend_parse_parameters_none() == FAILURE) { return; }
+  if (zend_parse_parameters_none() == FAILURE) { RETURN_FALSE; }
   zval *rv, *_items;
   _items = zend_read_property(
     vary_stack_ce,
@@ -41,6 +64,67 @@ PHP_METHOD(Stack, size)
     rv TSRMLS_CC
   );
   RETURN_LONG(zend_hash_num_elements(Z_ARRVAL_P(_items)));
+}
+
+PHP_METHOD(Stack, push)
+{
+  zval *_value, *rv, *_items, value;
+  ZEND_PARSE_PARAMETERS_START(1, 1)
+    Z_PARAM_ZVAL(_value)
+  ZEND_PARSE_PARAMETERS_END();
+  _items = zend_read_property(
+    vary_stack_ce,
+    getThis(),
+    "_items",
+    sizeof("_items") - 1,
+    0,
+    rv TSRMLS_CC
+  );
+  ZVAL_COPY(&value, _value);
+  uint32_t items_size = zend_hash_num_elements(Z_ARRVAL_P(_items));
+  if (zend_hash_next_index_insert(Z_ARRVAL_P(_items), &value) == NULL) {
+    zval_ptr_dtor(&value);
+    RETURN_FALSE;
+  }
+  zend_update_property(
+    vary_stack_ce,
+    getThis(),
+    "_items",
+    sizeof("_items") - 1,
+    _items TSRMLS_CC
+  );
+  RETURN_TRUE;
+}
+
+PHP_METHOD(Stack, pop)
+{
+  int num_args = ZEND_CALL_NUM_ARGS(execute_data);
+  if (num_args != 0) {
+    php_error_docref(
+      NULL,
+      E_WARNING,
+      "\\Vary\\Stack do not require any parameters"
+    );
+  }
+  zval *_items, *rv;
+  _items = zend_read_property(
+    vary_stack_ce,
+    getThis(),
+    "_items",
+    sizeof("_items") - 1,
+    0,
+    rv TSRMLS_CC
+  );
+  uint32_t items_size = zend_hash_num_elements(Z_ARRVAL_P(_items));
+  if (items_size == 0) {
+		RETURN_NULL();
+	}
+  Bucket *last_item = Z_ARRVAL_P(_items)->arData + items_size - 1;
+  zval return_item;
+  ZVAL_COPY(&return_item, &last_item->val);
+  zend_hash_del_bucket(Z_ARRVAL_P(_items), last_item);
+  zend_hash_internal_pointer_reset(Z_ARRVAL_P(_items));
+  RETURN_ZVAL(&return_item, 0, 1);
 }
 
 // PHP_METHOD(Stack, isEmpty)
@@ -65,29 +149,22 @@ PHP_METHOD(Stack, size)
 // }
 
 const zend_function_entry stack_functions[] = {
-  PHP_ME(Stack, __construct, arginfo_void, ZEND_ACC_PUBLIC)
+  PHP_ME(Stack, __construct, arginfo_array_init, ZEND_ACC_PUBLIC)
   PHP_ME(Stack, size, arginfo_void, ZEND_ACC_PUBLIC)
-  //PHP_ME(Stack, isEmpty, arginfo_void, ZEND_ACC_PUBLIC)
+  PHP_ME(Stack, push, arginfo_any, ZEND_ACC_PUBLIC)
+  PHP_ME(Stack, pop, arginfo_void, ZEND_ACC_PUBLIC)
 	PHP_FE_END
 };
 
 PHP_MINIT_FUNCTION(vary)
 {
 	/*
-   * Vary Classes for Data Structures:
+   * Vary - Data Structures:
    * Stack
 	 */
 	zend_class_entry stack_ce;
   INIT_NS_CLASS_ENTRY(stack_ce, "Vary", "Stack", stack_functions);
   vary_stack_ce = zend_register_internal_class(&stack_ce TSRMLS_CC);
-  // zval *stack_items = (zval *)malloc(sizeof(zval));
-  // zend_declare_property(
-  //   vary_stack_ce,
-  //   "_items",
-  //   sizeof("_items") - 1,
-  //   stack_items,
-  //   ZEND_ACC_PROTECTED TSRMLS_CC
-  // );
   return SUCCESS;
 }
 
@@ -99,9 +176,6 @@ PHP_RINIT_FUNCTION(vary)
 	return SUCCESS;
 }
 
-/*
- * Vary Helper functions:
- */
 static const zend_function_entry vary_functions[] = {
 	PHP_FE_END
 };
